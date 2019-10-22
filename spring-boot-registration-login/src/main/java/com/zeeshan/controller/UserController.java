da10cea2351979f7cc05448e97bc212d45050849
ZEESHAN ALAM KHAN <zeeshan.maths@gmail.com>
2019-10-22 17:22:08 +0530

    UserController class

--- spring-boot-registration-login/src/main/java/com/zeeshan/controller/UserController.java
+++ spring-boot-registration-login/src/main/java/com/zeeshan/controller/UserController.java
@@ -1,5 +1,4 @@
 package com.zeeshan.controller;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
